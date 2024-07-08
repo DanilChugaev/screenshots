a9d07b5ccf815f26b926f5fc0b49f7e875c4880e
Danil Chugaev <dmchugaev@gmail.com>
2024-07-08 14:47:39 +0700

    upd

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
+  timeout: 5 * 60 * 10000,
   use: {
-    viewport: { width: 1920, height: 2500 },
-    userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+    viewport: { width: 1600, height: 1700 },
+    // userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+    // userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 YaBrowser/24.4.5.506 Yowser/2.5 Safari/537.36',
+    // userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 YaBrowser/24.4.5.506 Yowser/2.5 Safari/537.36',
+    userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15',
   },
   // projects: [
   //   {
--- src/screen.test.ts
+++ src/screen.test.ts
@@ -2,15 +2,17 @@ import { test } from '@playwright/test';
 import requests from './config';
 
 const URLS = [
-    {
-        engine: 'google',
-        url: 'https://google.com/search?q',
-        params: '',
-    },
+    // {
+    //     engine: 'google',
+    //     url: 'https://google.com/search?q',
+    //     params: '',
+    // },
     {
         engine: 'yandex',
         url: 'https://yandex.ru/search/?text',
-        params: '&lr=65',
+        // params: '&lr=65',
+        params: '&lr=24353&search_source=yaru_desktop_common&search_domain=yaru',
+    //     https://ya.ru/search/?text=дизайнер+интерьера+обучение&lr=20103&search_source=yaru_desktop_common&search_domain=yaru
     },
 ]
 
@@ -19,23 +21,51 @@ function getRandomArbitrary(min: number, max: number) {
 }
 
 URLS.forEach(item => {
-    requests.forEach(request => {
-        const label = `Получаем скриншот ${item.engine}: ${request}`
+    for (let key of Object.keys(requests)) {
+        requests[key].forEach(request => {
+            const label = `Получаем скриншот type (${key}), engine (${item.engine}): ${request}`
 
-        test(label, async ({ page }) => {
-            const url = `${item.url}=${encodeURI(request)}${item.params}`;
+            test(label, async ({ page }) => {
+                // await page.goto('https://it-quest.pro/');
+                //
+                // await page.waitForTimeout(getRandomArbitrary(1000, 3000));
+                //
+                // await page.goto('https://my-js.org/docs/cheatsheet/mastering-ts/');
+                //
+                // await page.waitForTimeout(getRandomArbitrary(1000, 4000));
+                //
+                // await page.goto('https://www.google.com/search?q=%D1%8F%D0%BD%D0%B4%D0%B5%D0%BA%D1%81&oq=%D1%8F%D0%BD%D0%B4%D0%B5%D0%BA%D1%81&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQRRg7MgYIAhBFGEHSAQg3MzQ3ajBqN6gCALACAA&sourceid=chrome&ie=UTF-8');
+                //
+                // await page.waitForTimeout(getRandomArbitrary(1000, 5000));
+                //
+                // await page.goto('https://ya.ru/?utm_referrer=https%3A%2F%2Fwww.google.com%2F');
+                //
+                // await page.waitForTimeout(getRandomArbitrary(4000, 9000));
 
-            await page.goto(url);
+                // await page.getByRole('a').click({
+                //     modifiers: ['ControlOrMeta'],
+                //     position: { x: 0, y: 0 },
+                // });
 
-            await page.waitForTimeout(getRandomArbitrary(3000, 6000));
+                const url = `${item.url}=${encodeURI(request)}${item.params}`;
+                // const url = `${item.url}=${request.replace(/ /g, '+')}${item.params}`;
 
-            let path = `./screenshots/${item.engine}/${request}.png`;
+                console.log('url - ', url)
 
-            await page.screenshot({ path });
+                await page.waitForTimeout(getRandomArbitrary(4000, 9000));
 
-            console.log(`path - ${path}`);
+                await page.goto(url);
 
-            await page.waitForTimeout(getRandomArbitrary(7000, 31000));
-        });
-    })
+                await page.waitForTimeout(getRandomArbitrary(15000, 28000));
+
+                let path = `./screenshots/${key}/${item.engine}/${request}.png`;
+
+                console.log('path', path)
+
+                await page.screenshot({ path });
+
+                await page.waitForTimeout(getRandomArbitrary(12000, 23000));
+            });
+        })
+    }
 })
